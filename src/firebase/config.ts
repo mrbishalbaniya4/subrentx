7ee47e1bf1c0fc13e3bdebf97d3e237b3ce2a1c5
Bishal Baniya <mrbishalbaniya4@gmail.com>
2025-11-28 17:13:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3738901650-99839",
+  "appId": "1:477512883554:web:2aa0176fb454275ee4203b",
+  "apiKey": "AIzaSyCF1RpYzpYSGJ9uWmOm7VJC5dpxDfzCels",
+  "authDomain": "studio-3738901650-99839.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "477512883554"
+};
