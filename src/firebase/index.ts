a942f4004e84e945093c7c2c96678bb84e022498
Bishal Baniya <mrbishalbaniya4@gmail.com>
2025-11-28 20:12:38 +0000

    Try fixing this error: `Console Error: FirebaseError: Firestore has alre

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,60 +3,64 @@
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore'
+import { getFirestore, enableIndexedDbPersistence, Firestore } from 'firebase/firestore'
+
+let firestoreInstance: Firestore | null = null;
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
-  if (!getApps().length) {
-    // Important! initializeApp() is called without any arguments because Firebase App Hosting
-    // integrates with the initializeApp() function to provide the environment variables needed to
-    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
-    // without arguments.
-    let firebaseApp;
-    try {
-      // Attempt to initialize via Firebase App Hosting environment variables
-      firebaseApp = initializeApp();
-    } catch (e) {
-      // Only warn in production because it's normal to use the firebaseConfig to initialize
-      // during development
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
-      firebaseApp = initializeApp(firebaseConfig);
-    }
-
-    return getSdks(firebaseApp);
+  if (getApps().length) {
+    const app = getApp();
+    return getSdks(app);
   }
 
-  // If already initialized, return the SDKs with the already initialized App
-  return getSdks(getApp());
+  // Important! initializeApp() is called without any arguments because Firebase App Hosting
+  // integrates with the initializeApp() function to provide the environment variables needed to
+  // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
+  // without arguments.
+  let firebaseApp;
+  try {
+    // Attempt to initialize via Firebase App Hosting environment variables
+    firebaseApp = initializeApp();
+  } catch (e) {
+    // Only warn in production because it's normal to use the firebaseConfig to initialize
+    // during development
+    if (process.env.NODE_ENV === "production") {
+      console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
+    }
+    firebaseApp = initializeApp(firebaseConfig);
+  }
+
+  return getSdks(firebaseApp);
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
-  const firestore = getFirestore(firebaseApp);
-  // Enable offline persistence
-  try {
-    enableIndexedDbPersistence(firestore)
-      .catch((err) => {
-        if (err.code == 'failed-precondition') {
-          // Multiple tabs open, persistence can only be enabled
-          // in one tab at a a time.
-          console.warn('Firestore offline persistence failed: multiple tabs open.');
-        } else if (err.code == 'unimplemented') {
-          // The current browser does not support all of the
-          // features required to enable persistence
-           console.warn('Firestore offline persistence failed: browser does not support it.');
-        }
-      });
-  } catch (err) {
-      console.error("Error enabling offline persistence", err);
+  if (!firestoreInstance) {
+      firestoreInstance = getFirestore(firebaseApp);
+      // Enable offline persistence
+      try {
+        enableIndexedDbPersistence(firestoreInstance)
+          .catch((err) => {
+            if (err.code == 'failed-precondition') {
+              // Multiple tabs open, persistence can only be enabled
+              // in one tab at a a time.
+              console.warn('Firestore offline persistence failed: multiple tabs open.');
+            } else if (err.code == 'unimplemented') {
+              // The current browser does not support all of the
+              // features required to enable persistence
+              console.warn('Firestore offline persistence failed: browser does not support it.');
+            }
+          });
+      } catch (err) {
+          console.error("Error enabling offline persistence", err);
+      }
   }
 
 
   return {
     firebaseApp,
     auth: getAuth(firebaseApp),
-    firestore: firestore
+    firestore: firestoreInstance
   };
 }
 
@@ -64,6 +68,7 @@ export * from './provider';
 export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
+export * from './auth/use-user';
 export * from './non-blocking-updates';
 export * from './non-blocking-login';
 export * from './errors';
